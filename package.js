e7e030ce58559a9fcab6e271133efc02f53d17c2
Fabio Romita <romitafabio@gmail.com>
2018-05-05 17:17:22 +0200

    After publication on atmosphere.

--- package.js
+++ package.js
@@ -1,17 +1,18 @@
 Package.describe({
   name: "hotello:accounts-keycloak",
   summary: "Login service for Keycloak accounts",
-  version: "1.0.0"
+  version: "1.0.0",
+  git: "https://github.com/hotello/meteor-accounts-keycloak.git"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.5');
-  
+
   api.use('accounts-base', ['client', 'server']);
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('hotello:keycloak-oauth');
+  api.use('hotello:keycloak-oauth@1.0.0');
   api.imply('hotello:keycloak-oauth');
 
   api.addFiles("keycloak.js");
